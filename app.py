9870b5b3d7ae25f584a3458f1faa16ed932b01c6
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-04-26 19:02:36 +0100

    move database selection logic into db modules

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 from flask import Flask, render_template, request
-import db
+import db_coins, db_duties
 from db_duties import InMemoryDutyRepository, DatabaseDutyRepository
 from db_coins import InMemoryCoinRepository, DatabaseCoinRepository
 app = Flask(__name__)
-import json
-
-dabatase_location = "memory"
-
-if dabatase_location == "memory":
-  with open('seed_data/duties.json') as duties:
-      duties_repo = InMemoryDutyRepository(json.load(duties))
-  with open('seed_data/coins.json') as duties:
-      coins_repo = InMemoryCoinRepository(json.load(duties))
-
-elif dabatase_location == "real_db":
-  duties_repo = DatabaseDutyRepository()
-  coins_repo = DatabaseCoinRepository()
 
 
 @app.route('/')
 def index():
-  return render_template("index.html", coins=coins_repo.list_all_coins())
+  coins = db_coins.coins_repo.list_all_coins()
+  return render_template("index.html", coins=coins)
 
 @app.route('/automate')
 def automate():
@@ -29,6 +17,10 @@ def automate():
 @app.route('/security')
 def security():
   return render_template("security.html")
+
+@app.route('/houston')
+def houston():
+  return render_template("houston.html")
 
 # dynamic coin route to implement later
 @app.route('/coin/<id>')
