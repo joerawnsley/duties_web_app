6a0d38b35249ebe91027649ec62c4c7791015381
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-04-26 16:43:07 +0100

    add toggle for real database and in memory db

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
 import db
+from db_duties import InMemoryDutyRepository, DatabaseDutyRepository
+from db_coins import InMemoryCoinRepository, DatabaseCoinRepository
 app = Flask(__name__)
+import json
 
+dabatase_location = "memory"
+
+if dabatase_location == "memory":
+  with open('seed_data/duties.json') as duties:
+      duties_repo = InMemoryDutyRepository(json.load(duties))
+  with open('seed_data/coins.json') as duties:
+      coins_repo = InMemoryCoinRepository(json.load(duties))
+
+elif dabatase_location == "real_db":
+  duties_repo = DatabaseDutyRepository()
+  coins_repo = DatabaseCoinRepository()
 
 
 @app.route('/')
