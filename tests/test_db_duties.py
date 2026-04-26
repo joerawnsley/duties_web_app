967691f815ef512673d7de898f7bced5cb1ad115
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-04-26 13:18:03 +0100

    test for get duties by number

--- tests/test_db_duties.py
+++ tests/test_db_duties.py
@@ -13,3 +13,8 @@ def duties_repo():
 def test_repository_lists_all_duties(duties_repo):
     all_duties = duties_repo.list_all_duties()
     assert all_duties == seed_data
+    
+def test_repository_lists_single_specified_duty(duties_repo):
+    duty_2 = duties_repo.get_duties_by_number(2)
+    assert duty_2 == [{ "number": 2, "description": "Deploy continuously" }]
+    
